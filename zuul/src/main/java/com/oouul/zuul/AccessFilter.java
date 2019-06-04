f14dea6869ac2c24f5d12fe1c3960a1e4ed94dd2
lvtoo <o@oouul.com>
2019-06-04 21:14:03 +0800

    Eureka 集群部署完毕

---
+++ zuul/src/main/java/com/oouul/zuul/AccessFilter.java
@@ -0,0 +1,45 @@
+package com.oouul.zuul;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.http.HttpServletRequest;
+
+
+public class AccessFilter extends ZuulFilter {
+    private static Logger logger =  LoggerFactory.getLogger(AccessFilter.class);
+
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() {
+        RequestContext context = RequestContext.getCurrentContext();
+        HttpServletRequest request = context.getRequest();
+        logger.info(String.format("%s request to %s", request.getMethod(), request.getRequestURL().toString()));
+        Object accessToken = request.getParameter("accessToken");
+        if (accessToken == null){
+            logger.warn("access token is empty");
+            context.setSendZuulResponse(false);
+            context.setResponseStatusCode(401);
+            context.setResponseBody("你尚未登录，请登录后重试。");
+            return null;
+        }
+        return null;
+    }
+}
